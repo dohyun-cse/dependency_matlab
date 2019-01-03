97e930f898a7cbbebcb2855b70ad7fa0961a59d6
Dohyun Kim <kim92n@gmail.com>
2019-01-03 16:14:15 +0900

    First commit

---
+++ dispDep.m
@@ -0,0 +1,402 @@
+function [H, G] = dispDep(G_or_directory)
+% <SYNTAX>
+%
+% [H, G] = dispDep()
+% [H, G] = dispDep(directory)
+% [H, G] = dispDep(G)
+%
+% <DESCRIPTION>
+%
+% DISPDEP displays dependency plot from the table T and digraph G
+% when a file is clicked, that file and its children and parents will be
+% highlighted.
+%
+% [H, G] = dispDep() plot file hierarchy of current folder.
+%
+% [H, G] = dispDep(directory) plot file hierarchy of selected
+% directory including its subfolders.
+% 
+% H = dispDep(G) plot file hierachy of given graph G. This is useful
+% when G is already constructed by <genDependency>.
+% 
+% Input:
+%       directory
+%           Optional, string
+%           folder name
+%		G
+%			Optional, digraph
+%			dependency graph
+%       
+% 
+% Output:
+%		H
+%			plot handle
+%			graph
+%       G
+%           digraph
+%           dependency graph
+%
+% See also, GENDEPENDENCY, HIGHLIGHTFUN
+%
+%% RENAMED FROM : dispDependency, August 06, 2018
+%% DATE         : August 04, 2018
+%% VERSION      : 1.00 
+%% MATLAB ver.  : 9.4.0.813654 (R2018a)
+%% AUTHOR       : Dohyun Kim
+%% CONTACT      : kim92n@gmail.com
+
+%======================================================== end of definition
+%% PARSING
+
+if nargin > 1
+    error('Input arguments should be directory or digraph')
+end
+if nargin == 0 % if there is no input
+    G_or_directory = pwd;
+end
+if ischar(G_or_directory) % if it is a path
+    try
+        directory = strrep(G_or_directory, filesep, '/');
+        G = load([directory '/.dependency/G.mat']);
+        G = G.G;
+    catch
+        G = genDep(G_or_directory);
+    end
+elseif isa(G_or_directory,'digraph') % if it is a dependency
+    G = G_or_directory; % take it
+    directory = [];
+end
+
+%% DISPLAY
+
+fig = figure('windowstate','maximize');
+ax = axes(fig);
+
+h = plot(G,'nodelabel',G.Nodes.Short_Name);
+layout(h,'layered','Direction','right');
+matlabversion = ver('MATLAB');
+if matlabversion.Version >= 9.5
+    h.Interpreter = 'none';
+end
+
+title(ax, directory);
+
+% appearance
+ax.Title.Interpreter = 'none';
+axis(ax, 'tight');
+set(ax,'XColor','none');
+set(ax,'YColor','none');
+set(ax,'color','none');
+ax.Box = 'off';
+ax.Position(1) = 0;
+ax.Position(3) = 0.88;
+
+
+% highlight main scripts as pink
+ismain = contains(G.Nodes.Row, 'main');
+highlight(h,find(ismain),'nodecolor',[1,0.5,0.5],'markersize',12);
+
+% current original color
+orgNodeColor = h.NodeColor;
+orgMarkerSize = h.MarkerSize;
+orgEdgeColor = h.EdgeColor;
+orgLineWidth = h.LineWidth;
+
+% Set highlight function when it is pressed
+adj = adjacency(G);
+set(h, 'ButtonDownFcn', @(H, ~) highlightFun(h, adj, G, orgNodeColor, orgMarkerSize, orgEdgeColor, orgLineWidth));
+set(ax, 'ButtonDownFcn', @(~, ~) dehighlightFun(h, orgNodeColor, orgMarkerSize, orgEdgeColor, orgLineWidth));
+button1 = uicontrol('Parent',fig,'Style','pushbutton','string','Search', 'visible','on');
+button1.Callback = @(~,~) search_file(h, adj, G);
+
+if nargout
+    H = h;
+end
+
+end
+%%
+%========================================================== end of function
+
+function highlightFun(h, adj, G, orgNodeColor, orgMarkerSize, orgEdgeColor, orgLineWidth)
+    persistent  parentax subfig subax subh subG subid subnode
+    % subid: node id of subgraph G relative to G
+    % subnode : selected nodes to make subG
+    if isempty(parentax) % if function is first excuted
+        me = 1; % update current
+        parentax{me} = h.Parent; % 
+        subG = {}; subid = {[]}; subax = {}; subfig = {}; subnode = {[]};
+    else
+        me = [];
+        for i = 1 : length(parentax)
+            if eq(h.Parent,parentax{i})
+                me = i;
+            end
+        end
+        if isempty(me)
+            me = length(parentax)+1;
+            parentax{me} = h.Parent;
+            subnode{me} = [];
+        end
+    end
+
+    % if shift key is not pressed, remove highlight.
+    modifier = get(h.Parent.Parent, 'CurrentModifier');
+    if ~any(strcmp(modifier,'shift'))
+        dehighlightFun(h, orgNodeColor, orgMarkerSize, orgEdgeColor, orgLineWidth);
+    end
+    
+    % find selected node id
+    p = get(h.Parent, 'CurrentPoint'); % mouse click position
+    x = p(1,1); y = p(1,2);
+    [~,nodeID] = min((h.XData-x).^2/diff(get(h.Parent,'XLim'))^2+(h.YData-y).^2/diff(get(h.Parent,'YLim'))^2); % min distance node
+
+    % highlight selected node
+    highlight(h,nodeID,'nodecolor','r','markersize',15) % highlight current node
+
+    % get parents and children of current node
+    parents = highlightParents(h, nodeID, adj);
+    children = highlightChildren(h, nodeID, adj);
+    
+    % get short names
+    h.Parent.Title.String = G.Nodes(nodeID,:).Short_Name{1};
+    if any(strcmp(modifier,'shift')) % if shift key is pressed
+        if isempty(subnode{me}) % if there is no subnode
+            subnode{me} = 1; % set it to itself
+        else % otherwise
+            subnode{me} = [subnode{me}(:); length(subid{me}) + 1];
+        end
+        [subid{me}, ~, id] = unique([subid{me}(:);nodeID(:);parents(:);children(:)]); % append new node ID and its parents and children to subid
+        subnode{me} = id(subnode{me});
+    else
+        % same but whithout append
+        [subid{me},~,id] = unique([nodeID(:);parents(:);children(:)]);
+        subid{me} = round(subid{me});
+        subnode{me} = id(1);
+    end
+
+    if isempty(subG) % if there is no subgraph
+        subG = {}; % initialize with empty cell
+    end
+    subG{me} = subgraph(G, subid{me}); % make subgraph
+
+    if any(strcmp(modifier,'control')) % if control is pressed,
+        % new figure
+        if isempty(subfig) || length(subfig) < me || ~isvalid(subfig{me})
+            subfig{me} = figure;
+            screensize = get(0,'screensize');
+            subfig{me}.Position(1:2) = screensize(3:4)/4;
+            subfig{me}.Position(3:4) = screensize(3:4)/2;
+        end
+        
+        % new axis
+        if isempty(subax) || length(subax) < me || ~isvalid(subax{me})
+            subax{me} = axes(subfig{me});
+        end
+        
+        % draw subG
+        subh{me} = plot(subax{me}, subG{me},'nodelabel',subG{me}.Nodes.Short_Name);
+        layout(subh{me},'layered','Direction','right');
+        matlabversion = ver('MATLAB');
+        if matlabversion.Version >= 9.5
+            subh{me}.Interpreter = 'none';
+        end
+        
+        % highlight selected node
+        highlight(subh{me}, subnode{me}, 'nodecolor',[1,0.5,0.5],'markersize',15)
+
+        % appearance of axis
+        subax{me}.Title.Interpreter = 'none';
+        axis(subax{me}, 'tight');
+        set(subax{me},'XColor','none');
+        set(subax{me},'YColor','none');
+        set(subax{me},'color','none');
+        subax{me}.Box = 'off';
+        subax{me}.Position(1) = 0;
+        subax{me}.Position(3) = 0.88;
+        
+        % get current color
+        orgNodeColor = subh{me}.NodeColor;
+        orgMarkerSize = subh{me}.MarkerSize;
+        orgEdgeColor = subh{me}.EdgeColor;
+        orgLineWidth = subh{me}.LineWidth;
+        
+        % set highlight and dehighlight functions
+        set(subh{me}, 'ButtonDownFcn', @(H, ~) highlightFun(subh{me}, subG{me}.adjacency, subG{me}, orgNodeColor, orgMarkerSize, orgEdgeColor, orgLineWidth));
+        set(subax{me}, 'ButtonDownFcn', @(~, ~) dehighlightFun(subh{me}, orgNodeColor, orgMarkerSize, orgEdgeColor, orgLineWidth));
+        
+        % search button
+        button1 = uicontrol('Parent',subfig{me},'Style','pushbutton','string','Search', 'visible','on');
+        button1.Callback = @(~,~) search_file(subh{me}, subG{me}.adjacency, subG{me});
+        
+        % export button
+        button2 = uicontrol('Parent',subfig{me},'Style','pushbutton','string','export project', 'visible','on');
+        button2.Position(1) = 2*button1.Position(1) + button1.Position(3);
+        button2.Position(3) = button1.Position(3)*2;
+        button2.Callback = @(~,~) export_project(subG{me});
+        
+        % show persistent button
+        button3 = uicontrol('Parent',subfig{me},'Style','pushbutton','string','show persistent', 'visible','on');
+        button3.Position(1) = 2*button1.Position(1) + button1.Position(3) + button2.Position(3)*1.2;
+        button3.Position(3) = button1.Position(3)*2;
+        button3.Callback = @(~,~) show_persistent(subh{me}, subG{me});
+        
+    end
+    
+    drawnow;
+end
+%%
+%======================================================= end of subfunction
+
+
+function dehighlightFun(h, orgNodeColor, orgMarkerSize, orgEdgeColor, orgLineWidth)
+    % set to original values
+    h.NodeColor = orgNodeColor;
+    h.MarkerSize = orgMarkerSize;
+    h.EdgeColor = orgEdgeColor;
+    h.LineWidth = orgLineWidth;
+    if iscell(h.Parent.Title.String)
+        h.Parent.Title.String(2:end) = [];
+    end
+end
+%%
+% ======================================================= end of subfunction
+
+
+function newparents = highlightParents(h, nodeID, adj)
+
+    parents = false(size(adj,1),1);
+    parents(nodeID) = true;
+    while true
+        newparents = any(adj(parents,:),1); % nodes connected FROM current node
+        oldnum = nnz(parents); % current the number of nodes
+        parents = parents(:) | newparents(:); % update parents
+        if nnz(parents) == oldnum % 
+            break;
+        end
+    end
+    adj(~parents,:) = false;
+    [from, to] = find(adj);
+    
+    % highlight parent node and edge
+    highlight(h,to,'nodecolor','m','markersize',12);
+    highlight(h,from,to,'edgecolor','m','linewidth',2);
+    
+    newparents = unique(to);
+end
+%%
+%======================================================= end of subfunction
+
+
+function newchildren = highlightChildren(h, nodeID, adj)
+
+    children = false(size(adj,1),1);
+    children(nodeID) = true;
+    while true
+        newchildren = any(adj(:,children),2); % nodes connected FROM current node
+        oldnum = nnz(children); % current the number of nodes
+        children = children(:) | newchildren(:); % update parents
+        if nnz(children) == oldnum % 
+            break;
+        end
+    end
+    adj(:,~children) = false;
+    [from, to] = find(adj);
+    
+    % highlight parent node and edge
+    highlight(h,from,'nodecolor','b','markersize',12);
+    highlight(h,from,to,'edgecolor','b','linewidth',2);
+    
+    newchildren = unique(from);
+end
+%%
+%======================================================= end of subfunction
+
+
+function export_project(G)
+    % dialog setting
+    dlg_title = 'Export project';
+    num_lines = 1;
+    prompt = sprintf('Enter new project name.');
+    % get new folder name
+    newproj = char(inputdlg(prompt, dlg_title, num_lines));
+    if isempty(newproj)
+        warndlg('Invalid folder name.')
+        return;
+    end
+    % make directory
+    mkdir(newproj);
+    
+    % copy files
+    for i = 1 : height(G.Nodes)
+        try
+            copyfile([G.Nodes.Row{i}], ['./' newproj '/' G.Nodes.Row{i}]);
+        catch
+            s = strfind(G.Nodes.Row{i}, '/');
+            s = s(end);
+            mkdir([newproj '/' G.Nodes.Row{i}(1:s)]);
+            copyfile([G.Nodes.Row{i}], [newproj '/' G.Nodes.Row{i}]);
+        end
+    end
+    msgbox('Project successfully exported.','Export project');
+end
+%%
+%======================================================= end of subfunction
+
+
+function show_persistent(h, G)
+    
+    ispersistent = false(length(G.Nodes.Row),1); % init
+    for i = 1 : length(G.Nodes.Row) % for each file
+        try
+            fid = fopen(G.Nodes.Row{i}); % open file
+            s = fscanf(fid,'%c',inf);
+            if contains(s,'persistent') % if file contains persistent,
+                ispersistent(i) = true; % mark
+            end
+            fclose(fid);
+        catch
+            error('Error occured while reading file <%s>',G.Nodes.Row{i});
+        end
+    end
+    % highlight marked node as green
+    highlight(h, find(ispersistent), 'nodecolor','g','markersize',10);
+end
+%%
+%======================================================= end of subfunction
+
+
+function search_file(h, adj, G)
+
+    % dialog setting
+    dlg_prompt = {'Enter File Name:'};
+    dlg_title = 'Search';
+    % get filename
+    filename = inputdlg(dlg_prompt, dlg_title, 1);
+    
+    % find files which contains input filename
+    hasnames = find(contains(G.Nodes.Short_Name, filename));
+    switch numel(hasnames)
+        case 0 % if there is no
+            warndlg('There is no matching file. Try again'); % return warning
+            return
+            
+        case 1 % if there is
+            nodeID = hasnames; % select that one
+            
+        otherwise % if there are more than one file
+            % select among them
+            nodeID = hasnames(listdlg('PromptString', 'Select a file:', 'SelectionMode', 'single', 'ListString', G.Nodes.Short_Name(hasnames)));
+    end
+    
+    % highlight searched file
+    highlight(h,nodeID,'nodecolor','r','markersize',15) % highlight current node
+    highlightParents(h, nodeID, adj);
+    highlightChildren(h, nodeID, adj);
+    
+end
+%%
+%======================================================= end of subfunction
+
+
+%%
+%============================================================== end of file
---
+++ genDep.m
@@ -0,0 +1,191 @@
+function GG = genDep(directory)
+% <SYNTAX>
+%
+% genDep
+% genDep directory
+% G = genDep(directory);
+% G = genDep();
+% 
+% <DESCRIPTION>
+% 
+% GENDEP generates dependency graph for given directory including
+% its subfolder.
+% When GENDEP is called without input, generates dependency graph
+% for current folder.
+% 
+% Input:
+%		directory
+%			Optional, string, default = pwd
+%			target directory
+% 
+% Output:
+%		GG
+%			digraph
+%			directed graph (callee -> caller).
+%           GG.Nodes is a table of
+%           <relative path>  Short_Name  Date  Children
+%           where relative path does not contains its top level path.
+% 
+% See also, DISPDEPENDENCY
+% 
+%% RENAMED FROM : genDependency, August 06, 2018
+%% DATE         : August 06, 2018
+%% VERSION      : 1.00 
+%% MATLAB ver.  : 9.4.0.813654 (R2018a)
+%% AUTHOR       : Dohyun Kim
+%% CONTACT      : kim92n@gmail.com
+
+%=========================================================end of definition
+%%
+if ~nargin
+    directory = pwd;
+end
+%% PART 1. LOAD DEPENDENCY
+
+directory = strrep(directory, filesep, '/');
+oldpwd = pwd;
+cd(directory);
+try
+    G = load('./.dependency/G.mat','G'); % if exists
+    G = G.G;
+    fprintf('Dependency file exists. Update old files\n'); % found!
+    T = G.Nodes; % get table
+catch % if does not exist
+    fprintf('Dependency file does not exist.\nCreate new one.\n');
+    % create new table and graph
+    T = table({},{},{},'RowNames', {});
+    T.Properties.VariableNames = {'Short_Name', 'Date', 'Children'};
+    G = digraph([],T);
+end
+
+%% PART 2. CHECK UPDATE AND DELETE
+
+s = dir(sprintf('%s/**/*.m',strrep(pwd,filesep,'/'))); % get all matlab files
+
+newfuns = {s.name}.'; newfuns = strrep(newfuns,'.m',''); % function name
+newdates = cellfun(@(x) datetime(datevec(x)), {s.date}).'; % file creation dates
+newpaths = {s.folder}.'; % full paths
+newpaths = arrayfun(@(i) strrep(newpaths{i}, [pwd,filesep], ''), 1:length(newpaths),'UniformOutput',false);
+newpaths = arrayfun(@(i) strrep(newpaths{i}, pwd, ''), 1:length(newpaths),'UniformOutput',false);
+newpaths = arrayfun(@(i) strrep(newpaths{i}, filesep, '/'), 1:length(newpaths),'UniformOutput',false);
+newpaths = arrayfun(@(i) [newpaths{i} '/' newfuns{i} '.m'], 1:length(newfuns),'UniformOutput',false); % include file name
+for i = 1 : length(newpaths)
+    if strcmp(newpaths{i}(1),'/')
+        newpaths{i} = newpaths{i}(2:end);
+    end
+end
+
+oldFiles = T.Row; % get old file names
+isDeleted = true(length(oldFiles),1); % assume all file is deleted
+isUpToDate = false(length(oldFiles),1); % assume all file is not up to date
+for i = 1 : length(oldFiles) % for each file
+    newid = find(strcmp(newpaths,oldFiles{i})); % check if old file still exists
+    if ~isempty(newid) % if it exists,
+        isDeleted(i) = false; % it is not deleted
+        if T.Date(i) >= newdates(newid) % if it is not updated
+            isUpToDate(i) = true; % data is up to date
+            % delete from new files
+            newfuns(newid) = [];
+            newdates(newid) = [];
+            newpaths(newid) = [];
+%         else
+%             T.Short_Name(i)
+%             [T.Date(i) newdates(newid)]
+%             pause
+        end
+    end
+end
+
+adj = G.adjacency; % get previous adjacency
+hasDeletedChildren = any(adj(isDeleted,:),1); % find parents of deleted files
+if any(isUpToDate(hasDeletedChildren)) % if parents is not updated, there must be a problem.
+    % return error
+    s = ['Parents of deleted files are not updated.' newline];
+    s = [s, sprintf('%s\n',T.Row(~isUpToDate & hasDeletedChildren,:))];
+    error(s);
+end
+
+%% PART 3. GET DEPENDENCY DATA FOR NEW DATA
+
+% get dependency data
+isNotMethod = ~contains(newpaths,'@');
+isClass = false(size(isNotMethod));
+for i = 1 : length(isNotMethod)
+    if ~isNotMethod(i)
+        st = strfind(newpaths{i},'@');
+        st = st(end)+1;
+        ed = strfind(newpaths{i},'/');
+        ed = ed(ed>st);
+        ed = ed(1)-1;
+        if strcmp(newpaths{i}(st:ed),newfuns{i})
+            isNotMethod(i) = true;
+            isClass(i) = true;
+        end
+    end
+end
+newpaths = newpaths(isNotMethod);
+newfuns = newfuns(isNotMethod);
+newdates = newdates(isNotMethod);
+isClass = isClass(isNotMethod);
+
+fprintf('Generating Dependencies for %d files...', length(newfuns))
+fList = arrayfun(...
+    @(i) matlab.codetools.requiredFilesAndProducts(newpaths{i}, 'toponly'),  ...
+    1:length(newpaths), 'uniformoutput',false);
+fList = arrayfun(@(i) strrep(fList{i}, [pwd,filesep], ''), 1:length(fList),'UniformOutput',false);
+fList = arrayfun(@(i) strrep(fList{i}, pwd, ''), 1:length(fList),'UniformOutput',false);
+fList = arrayfun(@(i) strrep(fList{i}, filesep, '/'), 1:length(fList),'UniformOutput',false);
+for i = 1 : length(fList)
+    if isClass(i)
+        newfuns{i} = ['@',newfuns{i}];
+%         newpaths{i} = [newpaths{i}];
+    end
+end
+fprintf('\n');
+
+%% PART 4. CONSTRUCT TABLE AND GRAPH
+
+T = T(~isDeleted & isUpToDate,:); % remove deleted files from table.
+
+fprintf('Creating adjacency matrix...');
+
+% new table
+% newpaths
+T2 = table(newfuns(:), newdates(:), ...
+    arrayfun(@(i) strjoin(fList{i}, ', '), 1:length(fList), 'UniformOutput',false).', ...
+    'RowNames', newpaths);
+T2.Properties.VariableNames = {'Short_Name', 'Date', 'Children'};
+
+% prepare for update
+oldLength = height(T); % store old the number of files
+T = [T;T2]; % concatenate table
+[T,idx] = sortrows(T,'RowNames'); % sort by function name and get index
+adj = G.adjacency; % get old adjacency matrix
+adj = adj(~isDeleted & isUpToDate, ~isDeleted & isUpToDate);
+adj(height(T),height(T)) = 0; % adjust size for new graph
+
+adj = adj(idx,idx); % reorder adjacency matrix for new table
+[~,idx] = sort(idx);
+idx = idx(oldLength + 1 : end); % remove old data
+
+% Update adjacency matrix
+for i = 1 : height(T2)
+    for j = 1 : length(fList{i})
+        adj(strcmp(T.Row,fList{i}{j}),idx(i)) = true;
+    end
+end
+G = digraph(adj, T, 'OmitSelfLoops'); % renew graph
+fprintf('\n');
+
+if ~isfolder('./.dependency') % if folder does not exists
+    mkdir ./.dependency % create folder
+end
+save ./.dependency/G.mat G % save graph
+
+fprintf('Dependency generation is done.\n');
+fprintf('File is saved in <%s/.dependency/G.mat>\n',strrep(pwd,filesep,'/'));
+cd(oldpwd);
+if nargout
+    GG = G;
+end
+end
